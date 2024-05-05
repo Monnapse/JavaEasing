101a2ed8b798ddb565d675a9a1dc8b1a7961e7bf
Monnapse <masonshuber@gmail.com>
2024-05-05 10:25:37 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Easing TestTween = new Easing(EasingType.Bounce, EasingDirection.InOut, 2);
+        Easing TestTween = new Easing(EasingType.Sine, EasingDirection.InOut, 2);
         TestTween.SetValue(0, 5);
         //EaseInOutSine TestTween2 = new EaseInOutSine(0, 15, 30);
         //int cps = 1; // Cycles Per Second
@@ -18,7 +18,7 @@ public class Main {
                 break;
             } else if (TestTween.IsComplete()) {
                 System.out.println("COMPLETED");
-                //TestTween.ChangeValue(5);
+                TestTween.ChangeValue(5);
                 hasUpdated = true;
             }
             Thread.sleep(10);
